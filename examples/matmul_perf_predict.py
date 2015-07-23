c1bf56296f30c38ed4653f6411799673cb50526e
James Stevens <jdsteve2@illinois.edu>
2015-07-23 13:00:37 -0500

    added correctness check to matmul example

--- examples/matmul_perf_predict.py
+++ examples/matmul_perf_predict.py
@@ -13,36 +13,36 @@ from perf_model import GPUStats, KernelStats, ThreadConfig, PerfModel
 ctx = cl.create_some_context()
 queue = cl.CommandQueue(ctx, properties=cl.command_queue_properties.PROFILING_ENABLE)
 
-devices = ctx.get_info(cl.context_info.DEVICES)
-print "devices: \n", devices
+#print "devices: \n", ctx.get_info(cl.context_info.DEVICES)
 
-n = 2**12
+n = 2**10
 a_mat_dev = cl.clrandom.rand(queue, (n, n), dtype=np.float32)
 b_mat_dev = cl.clrandom.rand(queue, (n, n), dtype=np.float32)
 c_mat_dev = cl.clrandom.rand(queue, (n, n), dtype=np.float32)
 
 order = "C"
 knl = lp.make_kernel(
-	"{[i,j,k]: 0<=i,j,k<%d}" % n,
-	[
-	    "c[i, j] = sum(k, a[i, k]*b[k, j])"
-	    ],
-	[
-	    lp.GlobalArg("a", np.float32, shape=(n, n), order=order),
-	    lp.GlobalArg("b", np.float32, shape=(n, n), order=order),
-	    lp.GlobalArg("c", np.float32, shape=(n, n), order=order),
-	    ],
-	name="matmul")
+    "{[i,j,k]: 0<=i,j,k<%d}" % n,
+    [
+        "c[i, j] = sum(k, a[i, k]*b[k, j])"
+        ],
+    [
+        lp.GlobalArg("a", np.float32, shape=(n, n), order=order),
+        lp.GlobalArg("b", np.float32, shape=(n, n), order=order),
+        lp.GlobalArg("c", np.float32, shape=(n, n), order=order),
+        ],
+    name="matmul")
 
-ref_knl = knl  # TODO test result
+ref_knl = knl 
 
-knl = lp.split_iname(knl, "i", 16,
-	outer_tag="g.0", inner_tag="l.1")
-knl = lp.split_iname(knl, "j", 16,
-	outer_tag="g.1", inner_tag="l.0")
+knl = lp.split_iname(knl, "i", 16, outer_tag="g.0", inner_tag="l.1")
+knl = lp.split_iname(knl, "j", 16, outer_tag="g.1", inner_tag="l.0")
 knl = lp.split_iname(knl, "k", 16)
 knl = lp.add_prefetch(knl, "a", ["k_inner", "i_inner"])
 knl = lp.add_prefetch(knl, "b", ["j_inner", "k_inner", ])
+
+check = lp.auto_test_vs_ref(ref_knl, ctx, knl, print_code=True)
+#print "Correctness check: \n", check
 
 barrier_poly = get_barrier_poly(knl)
 barrier_count = barrier_poly.eval_with_dict({'n': n})
@@ -62,16 +62,16 @@ f32coal_s = sub_map.dict[
                     (np.dtype(np.float32), 'consecutive', 'store')
                     ].eval_with_dict({'n': n})
 f32coal = f32coal_l + f32coal_s
-print "coalesced: %i, (stores: %i, loads: %i)" % (f32coal, f32coal_s, f32coal_l)
+#print "coalesced: %i, (stores: %i, loads: %i)" % (f32coal, f32coal_s, f32coal_l)
 print "="*40
 
 # execute
 # -------
-print("running kernel...")
-knl = lp.set_options(knl, write_cl=True, highlight_cl=True)
+print "running kernel..."
+#knl = lp.set_options(knl, write_cl=True, highlight_cl=True)
 evt, (out,) = knl(queue, a=a_mat_dev, b=b_mat_dev, c=c_mat_dev)
 evt.wait()
-print("actual runtime: ", (evt.profile.END - evt.profile.START)*1e-9)
+print "actual runtime: ", (evt.profile.END - evt.profile.START)*1e-9
 
 gstats = GPUStats('TeslaK20')
 kstats = KernelStats(flops/(n*n), 0, f32coal/(n*n), barrier_count)
